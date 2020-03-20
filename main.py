d0ea7ae0eb661834e56b1cce24ca85f2b866818c
Daeruss <daerus21@yandex.ru>
2020-03-20 13:25:43 +0300

    master: Pre-commit test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+print('Пам')
+
 v_taxi = 45
 v_0 = v_taxi
 
@@ -10,7 +12,7 @@ km_into_meters = 1000./3600.
 S_max = 460
 t_max = S_max / (v_taxi * km_into_meters)
 
-print 'Maximum time = ', t_max
+print( 'Maximum time = ', t_max)
 
 l_taxi = 4.38
 distance = 10
@@ -22,7 +24,7 @@ delta_t = t[1]-t[0]
 v = np.ones(len(t)) * v_taxi
 
 a = 2 * (l + S_max - v_0 * km_into_meters * t_max) / (t_max**2)
-print "Acceleration = ", a
+print("Acceleration = ", a)
 
 V_find  =  v_0 + (a * t)/km_into_meters
 plt.plot(t, v)
@@ -34,7 +36,7 @@ plt.xlim(0, t_max + 10)
 plt.axvline(x=t_max)
 plt.show()
 
-print "Max speed = ", V_find[int(t_max / delta_t)]
+print("Max speed = ", V_find[int(t_max / delta_t)])
 
 S_taxi = np.zeros(len(t))
 S_taxi = km_into_meters * v_taxi * t
